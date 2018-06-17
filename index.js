a2d2ccdcab556e0eac75d7a990c892634dfab15c
Hasitha Liyanage <hasitha.liyanage@trycake.com>
2018-06-17 11:26:30 +0530

    replaced vorpal with readline

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const Jarvis = require('jarvis');
-const vorpal = require('vorpal')();
-const FAQClient = require('./faq');
+const FAQClient = require('./faq');   // business logic from here
+const Jarvis = require('jarvis');     // wrapped by jarvis 
+const readline = require('readline'); // and connected to a command line
 
 const app = new Jarvis();
 const client = new FAQClient();
 
+// register the command
 app.addCommand({
   command: 'getCountryPresident <country>',
   aliases: [
     'who is the president of <country>',
-    'president of <country>', 
     '<country> president'
   ],
   handler: async ({args: {country}}) => {
     const president = await client.getPresident(country);
-    return president 
-      ? `the president of ${country} is ${president}`
+    return president ? `the president of ${country} is ${president}`
       : `i don't know ${country}`;
   }
 });
 
-vorpal
-  .mode('faq')
-  .description('Enter FAQ mode')
-  .delimiter('faq>')
-  .action(async function(args, callback) {
-    let answer = await app.send(args);
-    vorpal.log(answer ? answer : 'I do not understand');
-    callback();
-  });
+// start the CLI
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'jarvis> '
+});
 
-vorpal
-  .delimiter('jarvis>')
-  .show();
-vorpal.exec('faq');
+rl.prompt();
 
-  
+// feed CLI input to the app, and app output back to CLI
+rl.on('line', async (line) => {
+  const res = await app.send(line.trim());
+  console.log(res ? `  ${res}` : '  I don\'t understand');
+  rl.prompt();
+});
+
+// TODO: error handling and other best practices
